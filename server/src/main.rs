706e958877366a506966a415a4599fb20a6c81f5
Darren Picard <darrenpicard25@gmail.com>
2022-11-26 22:07:30 -0700

    Working on automating actions

--- server/src/main.rs
+++ server/src/main.rs
@@ -43,7 +43,6 @@ struct Opt {
 #[tokio::main]
 async fn main() {
     let opt = Opt::parse();
-
     // Setup logging & RUST_LOG from args
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", format!("{},hyper=info,mio=info", opt.log_level))
